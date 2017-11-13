788ba81e6a41212d3438bafa8460282df65c25c7
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-13 16:51:24 +0100

    routes restaurants and action restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'items#index'
+root to: 'items#index'
+
+  resources :restaurants, only: [:show, :index] do
+    resources :items, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
