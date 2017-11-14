bb0c2a81e65501ebf310596cf1f8613025138e59
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-14 19:54:18 +0100

    order via the menu - DONE, basket DONE

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :set_table, only: [:index]
+
   def index
     @restaurant = Restaurant.first
     @items = @restaurant.items
@@ -6,5 +8,25 @@ class ItemsController < ApplicationController
     @items_burgers = @items.where(category: 'burgers')
     @items_sides = @items.where(category: 'sides')
     @items_drinks = @items.where(category: 'drinks')
+
+    @orderline = Orderline.new
+    @order = current_order
+  end
+
+  private
+
+  def set_table
+    @table = Table.find(params[:table_id])
+  end
+
+  def current_order
+    unless session[:order_id]
+      order = Order.new(table: @table, status: "order in session")
+      order.save!
+      session[:order_id] = order.id
+      order
+    else
+      Order.find(session[:order_id])
+    end
   end
 end
--- app/models/orderline.rb
+++ app/models/orderline.rb
@@ -1,4 +1,6 @@
 class Orderline < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+  validates :item_id, presence: true
 end
