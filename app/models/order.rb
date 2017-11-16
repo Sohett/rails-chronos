b19214aff71ff64b9549a5a52977df09a36bb18e
Luc Jerome <lucjerome@live.be>
2017-11-16 15:03:04 +0100

    basket summary implemented

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,21 @@
 class Order < ApplicationRecord
   belongs_to :table
   has_many :orderlines
+
+  def quantity
+    result = 0
+    self.orderlines.each do |orderline|
+      result += orderline.quantity
+    end
+    result
+  end
+
+  def total_price
+    total_price = 0
+    self.orderlines.each do |orderline|
+      price = orderline.quantity * orderline.item.price
+      total_price += price
+    end
+    return total_price
+  end
 end
