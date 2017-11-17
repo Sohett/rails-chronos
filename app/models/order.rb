5e0bf164ddddabc1aad8c54107ad25a572efcde4
Alex Vacca <alex.vacca@hotmail.com>
2017-11-17 16:21:12 +0100

    design friday before presentation

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
   belongs_to :table
   has_many :orderlines
 
+  def price_per_order
+    price = 0
+    self.orderlines.each do |orderline|
+      price += orderline.quantity * orderline.item.price
+    end
+    return price
+  end
+
+
 end
