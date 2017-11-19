85672b39615ac6ac3c0e1950d6629eef6b719fbd
Thomas Sohet <sohet.Thomas@gmail.com>
2017-11-19 21:31:56 +0100

    seed for multiple restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,10 @@ Table.destroy_all
 puts "creating 2 restaurant2"
 resto1 = Restaurant.new(name: "Be Burger", address: "Hector Henneaulaan 164, 1930 Zaventem")
 resto2 = Restaurant.new(name: "Le schievelavabo", address: "Rue du Collège St Michel 52, 1150 Woluwe-Saint-Pierre")
+resto3 = Restaurant.new(name: "Il classico", address: "Avenue Emile de Beco 25, 1050 Bruxelles")
 resto1.save!
 resto2.save!
+resto3.save!
 puts "resto #{resto1.id} & #{resto2.id} created"
 
 
@@ -40,6 +42,15 @@ table16 = Table.new(table_number: "6",capacity: 8)
 table17 = Table.new(table_number: "7",capacity: 2)
 table18 = Table.new(table_number: "8",capacity: 4)
 
+table19 = Table.new(table_number: "1",capacity: 14)
+table20 = Table.new(table_number: "2",capacity: 11)
+table21 = Table.new(table_number: "3",capacity: 20)
+table22 = Table.new(table_number: "4",capacity: 5)
+table23 = Table.new(table_number: "5",capacity: 8)
+table24 = Table.new(table_number: "6",capacity: 8)
+table25 = Table.new(table_number: "7",capacity: 2)
+table26 = Table.new(table_number: "8",capacity: 4)
+
 table1.restaurant = resto1
 table2.restaurant = resto1
 table3.restaurant = resto1
@@ -60,6 +71,15 @@ table16.restaurant = resto2
 table17.restaurant = resto2
 table18.restaurant = resto2
 
+table19.restaurant = resto3
+table20.restaurant = resto3
+table21.restaurant = resto3
+table22.restaurant = resto3
+table23.restaurant = resto3
+table24.restaurant = resto3
+table25.restaurant = resto3
+table26.restaurant = resto3
+
 table1.save!
 table2.save!
 table3.save!
@@ -78,6 +98,14 @@ table15.save!
 table16.save!
 table17.save!
 table18.save!
+table19.save!
+table20.save!
+table21.save!
+table22.save!
+table23.save!
+table24.save!
+table25.save!
+table26.save!
 
 
 #################################################################################################
@@ -191,5 +219,82 @@ plat2.save!
 plat3 = Item.new(name: "Tagliatelles aux scampi sauce diable", description: "", price: 14.5, category: "PASTA")
 plat3.restaurant = resto2
 plat3.save!
+
+
+######################################################################################################################
+# IL CLASSICO
+######################################################################################################################
+
+classico1 = Item.new(name: "Toast Champignons", description: "", price: 8.0, category: "Les Entrées")
+classico1.restaurant = resto3
+classico1.save!
+
+classico2 = Item.new(name: "Calamars Frits Tartare", description: "", price: 13.0, category: "Les Entrées")
+classico2.restaurant = resto3
+classico2.save!
+
+classico3 = Item.new(name: "Carpaccio de Boeuf", description: "", price: 13.5, category: "Les Entrées")
+classico3.restaurant = resto3
+classico3.save!
+
+classico4 = Item.new(name: "Aubergines Parmigiana", description: "", price: 12.0, category: "Les Entrées")
+classico4.restaurant = resto3
+classico4.save!
+
+classico5 = Item.new(name: "Penne Arrabiata", description: "", price: 9.5, category: "Les Pâtes")
+classico5.restaurant = resto3
+classico5.save!
+
+classico5 = Item.new(name: "Penne Quattro Formaggi", description: "", price: 13.0, category: "Les Pâtes")
+classico5.restaurant = resto3
+classico5.save!
+
+classico6 = Item.new(name: "Tortellini San Daniele", description: "", price: 14.0, category: "Les Pâtes")
+classico6.restaurant = resto3
+classico6.save!
+
+classico7 = Item.new(name: "Tagliatelle au Saumon", description: "", price: 14.0, category: "Les Pâtes")
+classico7.restaurant = resto3
+classico7.save!
+
+classico8 = Item.new(name: "Spaghetti Vongole fraiche", description: "", price: 19.5, category: "Les Pâtes")
+classico8.restaurant = resto3
+classico8.save!
+
+classico9 = Item.new(name: "Margherita", description: "(Tomate, mozzarella)", price: 8.0, category: "Les Pizzas")
+classico9.restaurant = resto3
+classico9.save!
+
+classico10 = Item.new(name: "Ardi", description: "(Tomate, mozzarella, champignons)", price: 9.0, category: "Les Pizzas")
+classico10.restaurant = resto3
+classico10.save!
+
+classico11 = Item.new(name: "Zoni", description: "(Tomate mozzarella, jambon)", price: 9.5, category: "Les Pizzas")
+classico11.restaurant = resto3
+classico11.save!
+
+classico12 = Item.new(name: "Siciliana", description: "(Tomate, mozzarella, jambon, salami piquant)", price: 9.0, category: "Les Pizzas")
+classico12.restaurant = resto3
+classico12.save!
+
+classico13 = Item.new(name: "Napoletana", description: "(Tomate, mozzarella, anchois, olives, câpres)", price: 9.5, category: "Les Pizzas")
+classico13.restaurant = resto3
+classico13.save!
+
+classico14 = Item.new(name: "Capricciosa", description: "(Tomate, mozzarella, jambon, poivrons, olives, salami piquant)", price: 10.5, category: "Les Pizzas")
+classico14.restaurant = resto3
+classico14.save!
+
+classico15 = Item.new(name: "Dragani Montepulciano d’Abbruzzo", description: "", price: 19.0, category: "Vins rouges")
+classico15.restaurant = resto3
+classico15.save!
+
+classico16 = Item.new(name: "Chianti Ruffino", description: "", price: 21.0, category: "Vins rouges")
+classico16.restaurant = resto3
+classico16.save!
+
+classico17 = Item.new(name: "Lodola Nuova Ruffino", description: "", price: 29.0, category: "Vins rouges")
+classico17.restaurant = resto3
+classico17.save!
 
 puts"finished!"
